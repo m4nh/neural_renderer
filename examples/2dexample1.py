bd7deddd35a96898a127efc17977bdca1b9d8554
Daniele De Gregorio <degregorio.daniele@gmail.com>
2019-11-20 19:32:42 -0500

    ciao

--- examples/2dexample1.py
+++ examples/2dexample1.py
@@ -15,36 +15,53 @@ import imageio
 import time
 import cv2
 
-image1 = 'data/eye.png'
+
+class Morph(nn.Module):
+    def __init__(self, source, target):
+        super(Morph, self).__init__()
+
+        # self.source = nn.Parameter(source)
+        self.register_buffer("source", source)
+        self.register_buffer("target", target)
+        self.flow = nn.Parameter(self.source)
+
+        self.s1 = nn.Sigmoid()
+        self.s2 = nn.Sigmoid()
+
+    def getImage(self):
+        return (np.clip((self.flow).permute(1, 2, 0).detach().numpy(), 0., 1.) * 255.).astype(
+            np.uint8)
+
+    def forward(self):
+        loss =  torch.sum(torch.abs(self.flow - self.target)) + 0.1*torch.sum(torch.abs(self.flow - self.source))
+        return loss
+
+
+image1 = 'data/eye_side.png'
 image2 = 'data/eye_flip.png'
 
 image_ref_1 = torch.from_numpy(imread(image1).astype('float32') / 255.).permute(2, 0, 1)[:3, ::]
 image_ref_2 = torch.from_numpy(imread(image2).astype('float32') / 255.).permute(2, 0, 1)[:3, ::]
 
-source = nn.Parameter(image_ref_1)
-flow = nn.Parameter(torch.zeros_like(image_ref_1))
+source = nn.Parameter(image_ref_1, requires_grad=True)
 target = image_ref_2
 
-optimizer = torch.optim.Adam([source], lr=0.0001)
+model = Morph(source, target)
+
+optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
 
 loop = tqdm.tqdm(range(15000))
-
-
-s = nn.Sigmoid()
 
 for i in loop:
     loop.set_description('Optimizing')
     # print(source.shape)
 
-    flow_a = s(flow)
-
-    source_d = source + flow_a
-
-    loss = torch.sum(torch.abs(source_d - target))
-    loss.backward()
+    loss = model()
+    loss.backward(retain_graph=True)
     optimizer.step()
+    print(loss)
 
-    out = (source_d.permute(1, 2, 0).detach().numpy() * 255.).astype(np.uint8)
+    out = model.getImage()
     if i % 10 == 0:
-        cv2.imshow("out",out)
+        cv2.imshow("out", out)
         cv2.waitKey(1)
---
+++ examples/example7.py
@@ -0,0 +1,244 @@
+"""
+Example 2. Optimizing vertices.
+"""
+from __future__ import division
+import os
+import argparse
+import glob
+
+import torch
+import torch.nn as nn
+import numpy as np
+from skimage.io import imread, imsave
+import tqdm
+import imageio
+
+import neural_renderer as nr
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(current_dir, 'data')
+
+images = {
+    'data/eye_flip.png': [3, 180, 0],
+    'data/eye_side.png': [3, 270, 0],
+    'data/eye.png': [3, 0, 0],
+}
+
+
+class LaplacianLoss(nn.Module):
+    def __init__(self, vertex, faces, average=False):
+        super(LaplacianLoss, self).__init__()
+        self.nv = vertex.size(0)
+        self.nf = faces.size(0)
+        self.average = average
+        laplacian = np.zeros([self.nv, self.nv]).astype(np.float32)
+
+        laplacian[faces[:, 0], faces[:, 1]] = -1
+        laplacian[faces[:, 1], faces[:, 0]] = -1
+        laplacian[faces[:, 1], faces[:, 2]] = -1
+        laplacian[faces[:, 2], faces[:, 1]] = -1
+        laplacian[faces[:, 2], faces[:, 0]] = -1
+        laplacian[faces[:, 0], faces[:, 2]] = -1
+
+        r, c = np.diag_indices(laplacian.shape[0])
+        laplacian[r, c] = -laplacian.sum(1)
+
+        for i in range(self.nv):
+            laplacian[i, :] /= laplacian[i, i]
+
+        self.register_buffer('laplacian', torch.from_numpy(laplacian))
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = torch.matmul(self.laplacian, x)
+        dims = tuple(range(x.ndimension())[1:])
+        x = x.pow(2).sum(dims)
+        if self.average:
+            return x.sum() / batch_size
+        else:
+            return x
+
+
+class FlattenLoss(nn.Module):
+    def __init__(self, faces, average=False):
+        super(FlattenLoss, self).__init__()
+        self.nf = faces.size(0)
+        self.average = average
+
+        faces = faces.detach().cpu().numpy()
+        vertices = list(set([tuple(v) for v in np.sort(np.concatenate((faces[:, 0:2], faces[:, 1:3]), axis=0))]))
+
+        v0s = np.array([v[0] for v in vertices], 'int32')
+        v1s = np.array([v[1] for v in vertices], 'int32')
+        v2s = []
+        v3s = []
+        for v0, v1 in zip(v0s, v1s):
+            count = 0
+            for face in faces:
+                if v0 in face and v1 in face:
+                    v = np.copy(face)
+                    v = v[v != v0]
+                    v = v[v != v1]
+                    if count == 0:
+                        v2s.append(int(v[0]))
+                        count += 1
+                    else:
+                        v3s.append(int(v[0]))
+        v2s = np.array(v2s, 'int32')
+        v3s = np.array(v3s, 'int32')
+
+        self.register_buffer('v0s', torch.from_numpy(v0s).long())
+        self.register_buffer('v1s', torch.from_numpy(v1s).long())
+        self.register_buffer('v2s', torch.from_numpy(v2s).long())
+        self.register_buffer('v3s', torch.from_numpy(v3s).long())
+
+    def forward(self, vertices, eps=1e-6):
+        # make v0s, v1s, v2s, v3s
+        batch_size = vertices.size(0)
+
+        v0s = vertices[:, self.v0s, :]
+        v1s = vertices[:, self.v1s, :]
+        v2s = vertices[:, self.v2s, :]
+        v3s = vertices[:, self.v3s, :]
+
+        a1 = v1s - v0s
+        b1 = v2s - v0s
+        a1l2 = a1.pow(2).sum(-1)
+        b1l2 = b1.pow(2).sum(-1)
+        a1l1 = (a1l2 + eps).sqrt()
+        b1l1 = (b1l2 + eps).sqrt()
+        ab1 = (a1 * b1).sum(-1)
+        cos1 = ab1 / (a1l1 * b1l1 + eps)
+        sin1 = (1 - cos1.pow(2) + eps).sqrt()
+        c1 = a1 * (ab1 / (a1l2 + eps))[:, :, None]
+        cb1 = b1 - c1
+        cb1l1 = b1l1 * sin1
+
+        a2 = v1s - v0s
+        b2 = v3s - v0s
+        a2l2 = a2.pow(2).sum(-1)
+        b2l2 = b2.pow(2).sum(-1)
+        a2l1 = (a2l2 + eps).sqrt()
+        b2l1 = (b2l2 + eps).sqrt()
+        ab2 = (a2 * b2).sum(-1)
+        cos2 = ab2 / (a2l1 * b2l1 + eps)
+        sin2 = (1 - cos2.pow(2) + eps).sqrt()
+        c2 = a2 * (ab2 / (a2l2 + eps))[:, :, None]
+        cb2 = b2 - c2
+        cb2l1 = b2l1 * sin2
+
+        cos = (cb1 * cb2).sum(-1) / (cb1l1 * cb2l1 + eps)
+
+        dims = tuple(range(cos.ndimension())[1:])
+        loss = (cos + 1).pow(2).sum(dims)
+        if self.average:
+            return loss.sum() / batch_size
+        else:
+            return loss
+
+
+class Model(nn.Module):
+    def __init__(self, filename_obj, filename_ref):
+        super(Model, self).__init__()
+
+        # load .obj
+        vertices, faces = nr.load_obj(filename_obj)
+        self.vertices = nn.Parameter(vertices[None, :, :])
+        self.register_buffer('faces', faces[None, :, :])
+
+        # create textures
+        texture_size = 4
+        textures = torch.zeros(1, self.faces.shape[1], texture_size, texture_size, texture_size, 3, dtype=torch.float32)
+        self.textures = nn.Parameter(textures)
+
+        # load reference image
+        counter = 0
+        for k, v in images.items():
+            image_ref = torch.from_numpy(imread(k).astype('float32').mean(-1) / 255.)[None, :3, ::]
+            print("IAMGE REF", image_ref.shape)
+            # image_ref = torch.from_numpy(imread(k).astype(np.float32).mean(-1) / 255.)[None, ::]
+            self.register_buffer('image_ref_{}'.format(counter), image_ref)
+            counter += 1
+
+        # setup renderer
+        renderer = nr.Renderer(camera_mode='look_at')
+        self.renderer = renderer
+
+        # define Laplacian and flatten geometry constraints
+        self.laplacian_loss = LaplacianLoss(self.vertices[0].cpu(), self.faces[0].cpu())
+        self.flatten_loss = FlattenLoss(self.faces[0].cpu())
+
+    def forward(self):
+        loss = 0.0
+        counter = 0
+        for k, v in images.items():
+            self.renderer.eye = nr.get_points_from_angles(v[0], v[2], v[1])
+            # image, _, _ = self.renderer(self.vertices, self.faces, torch.tanh(self.textures))
+            image = self.renderer(self.vertices, self.faces, mode='silhouettes')
+            # image = self.renderer(self.vertices, self.faces, mode='silhouettes')
+            loss += torch.sum((image - getattr(self, 'image_ref_{}'.format(counter))) ** 2)
+            counter += 1
+
+        laplacian_loss = self.laplacian_loss(self.vertices).mean()
+        flatten_loss = self.flatten_loss(self.vertices).mean()
+
+        return loss + 1 * laplacian_loss + 1 * flatten_loss
+
+
+def make_gif(filename):
+    with imageio.get_writer(filename, mode='I') as writer:
+        for filename in sorted(glob.glob('/tmp/_tmp_*.png')):
+            writer.append_data(imageio.imread(filename))
+            os.remove(filename)
+    writer.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-io', '--filename_obj', type=str, default=os.path.join(data_dir, 'sphere.obj'))
+    parser.add_argument('-ir', '--filename_ref', type=str, default=os.path.join(data_dir, 'example2_ref.png'))
+    parser.add_argument(
+        '-oo', '--filename_output_optimization', type=str, default=os.path.join(data_dir, 'example2_optimization.gif'))
+    parser.add_argument(
+        '-or', '--filename_output_result', type=str, default=os.path.join(data_dir, 'example2_result.gif'))
+    parser.add_argument('-g', '--gpu', type=int, default=0)
+    args = parser.parse_args()
+
+    model = Model(args.filename_obj, args.filename_ref)
+    model.cuda()
+
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()))
+
+    # optimizer.setup(model)
+    loop = tqdm.tqdm(range(5000))
+    for i in loop:
+        loop.set_description('Optimizing')
+        # optimizer.target.cleargrads()
+        optimizer.zero_grad()
+        loss = model()
+        print(loss)
+        loss.backward()
+        optimizer.step()
+        if i % 20 == 0:
+            # images, _, _ = model.renderer(model.vertices, model.faces, model.textures)
+            # image = images.detach().cpu().numpy()[0].transpose((1, 2, 0))
+            images = model.renderer(model.vertices, model.faces, mode='silhouettes')
+            image = images.detach().cpu().numpy()[0]
+            imsave('/tmp/_tmp_%04d.png' % i, image)
+    make_gif(args.filename_output_optimization)
+
+    # draw object
+    loop = tqdm.tqdm(range(0, 360, 4))
+    for num, azimuth in enumerate(loop):
+        loop.set_description('Drawing')
+        model.renderer.eye = nr.get_points_from_angles(2.732, 0, azimuth)
+        # images, _, _ = model.renderer(model.vertices, model.faces, model.textures)
+        # image = images.detach().cpu().numpy()[0].transpose((1, 2, 0))
+        images = model.renderer(model.vertices, model.faces, mode='silhouettes')
+        image = images.detach().cpu().numpy()[0]
+        imsave('/tmp/_tmp_%04d.png' % num, image)
+    make_gif(args.filename_output_result)
+
+
+if __name__ == '__main__':
+    main()
