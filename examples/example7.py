f30844ce964477f8ee49ba831db72dbe2d9f9400
Daniele De Gregorio <degregorio.daniele@gmail.com>
2019-11-20 19:35:08 -0500

    ciao

--- examples/example7.py
+++ examples/example7.py
@@ -165,8 +165,8 @@ class Model(nn.Module):
         self.renderer = renderer
 
         # define Laplacian and flatten geometry constraints
-        self.laplacian_loss = LaplacianLoss(self.vertices[0].cpu(), self.faces[0].cpu())
-        self.flatten_loss = FlattenLoss(self.faces[0].cpu())
+        # self.laplacian_loss = LaplacianLoss(self.vertices[0].cpu(), self.faces[0].cpu())
+        # self.flatten_loss = FlattenLoss(self.faces[0].cpu())
 
     def forward(self):
         loss = 0.0
@@ -179,10 +179,10 @@ class Model(nn.Module):
             loss += torch.sum((image - getattr(self, 'image_ref_{}'.format(counter))) ** 2)
             counter += 1
 
-        laplacian_loss = self.laplacian_loss(self.vertices).mean()
-        flatten_loss = self.flatten_loss(self.vertices).mean()
+        # laplacian_loss = self.laplacian_loss(self.vertices).mean()
+        # flatten_loss = self.flatten_loss(self.vertices).mean()
 
-        return loss + 1 * laplacian_loss + 1 * flatten_loss
+        return loss #+ 1 * laplacian_loss + 1 * flatten_loss
 
 
 def make_gif(filename):
